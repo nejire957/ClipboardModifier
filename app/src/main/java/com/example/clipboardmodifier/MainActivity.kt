c4bf1a34b24aedb2cdcef3736df66192656ade24
nejire957 <nejiretuxing@gmail.com>
2022-04-08 21:15:28 +0900

    とりあえず動くようになった

--- app/src/main/java/com/example/clipboardmodifier/MainActivity.kt
+++ app/src/main/java/com/example/clipboardmodifier/MainActivity.kt
@@ -6,31 +6,48 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.clipboardmodifier.databinding.ActivityMainBinding
 import android.content.ClipboardManager
 import android.content.ClipData
+import com.google.android.material.snackbar.Snackbar
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*
-        val clipboardManager = applicationContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboardManager.addPrimaryClipChangedListener {
-            val item = clipboardManager.primaryClip?.getItemAt(0)
-            if(item != null){
-                val text = item.coerceToText(applicationContext).toString()
-                clipboardManager.setPrimaryClip(ClipData.newPlainText("", text + "a"))
-            }
-        }
-        */
-
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.addPrimaryClipChangedListener(MyOnPrimaryClipChangedListener(clipboard))
+
         binding.button.setOnClickListener {
-            binding.label.text = "Clicked"
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clippedText = clipboard?.primaryClip?.getItemAt(0)?.text?.toString().orEmpty()
+            val modifiedText = clippedText.replace(Regex("\r\n|\n"), "_")
+            binding.plainText.setText(modifiedText)
+        }
+
+        binding.button2.setOnClickListener{
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clippedText = clipboard?.primaryClip?.getItemAt(0)?.text?.toString().orEmpty()
+            val modifiedText = clippedText.replace(Regex("\r\n|\n"), " ")
+            val clipItem = ClipData.newPlainText("modified text", modifiedText)
+            clipboard?.setPrimaryClip(clipItem)
+        }
+    }
+}
+
+class MyOnPrimaryClipChangedListener(val clipboard: ClipboardManager): ClipboardManager.OnPrimaryClipChangedListener{
+    private var modifiedText = ""
+    override fun onPrimaryClipChanged() {
+        val clippedText = clipboard.primaryClip?.getItemAt(0)?.text?.toString().orEmpty()
+        if(clippedText != modifiedText) {
+            modifiedText = clippedText.replace(Regex("\r\n|\n"), " ")
+            val clipItem = ClipData.newPlainText("modified text", modifiedText)
+            clipboard?.setPrimaryClip(clipItem)
         }
     }
 }
